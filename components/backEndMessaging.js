b2b635586c39e9407ef3b59f93765ccb65187beb
santiagocasti <santiago.castineira@gmail.com>
2014-06-09 19:56:20 +0200

    Class for handling messaging in the backend.

---
+++ components/backEndMessaging.js
@@ -0,0 +1,50 @@
+var BackEndMessaging = (function () {
+
+
+    function sendMessage(message, onResponse) {
+        var msg = MessagePassing.PrepareMessage(message);
+        chrome.runtime.sendMessage(msg, onResponse);
+    }
+
+    function shouldProcess(message) {
+        return message.to == MessagePassing.Destinations.BACK;
+    }
+
+    return {
+
+        handleMessage: function (rawMessage, sender, sendResponse) {
+
+            debug("BackEndMessaging handleMessage");
+
+            // make sure we should process this message
+            if (!shouldProcess(rawMessage)) {
+                debug("We should not process this message:", rawMessage);
+                return;
+            }
+
+            debug("We should process this message:");
+            debug(rawMessage);
+
+            var message = MessagePassing.ParseMessage(rawMessage);
+
+            /** @var message MessagePassing */
+            switch (message.type) {
+                case MessagePassing.MessageTypes.NEW_DATA_CREATED:
+                    debug("Received a message of type NEW_DATA_CREATED: ", message);
+
+                    var appController = ApplicationController.getInstance();
+                    appController.newDataCreated(message.content);
+
+                    break;
+                default:
+                    debug("Received message that should not handle [" + message.type + "]: ", message);
+            }
+
+        },
+
+        sendMessage: function (message, onResponse) {
+            return sendMessage(message, onResponse);
+        }
+
+    };
+})();
