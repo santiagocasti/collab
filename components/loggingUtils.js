bf438b18e721df83e41507dd6e7147cd54118865
santiagocasti <santiago.castineira@gmail.com>
2014-06-09 19:22:10 +0200

    More functionality to have a debug function.

--- components/loggingUtils.js
+++ components/loggingUtils.js
@@ -1,14 +1,44 @@
-function log(message, object){
+const DEBUG_MODE_ON = true;
+
+function createMsgHead(depth, debug) {
     var e = new Error;
-    var line = e.stack.split('\n')[2];
+    var line = e.stack.split('\n')[depth];
     var re = RegExp('[^ ]*$');
     var fileFullpathString = re.exec(line)[0];
     var re2 = RegExp("([^/]*)$");
     var filenameAndLineNumber = re2.exec(fileFullpathString)[1];
     var lineNumber = filenameAndLineNumber.split(":")[1];
     var filename = filenameAndLineNumber.split(":")[0];
-    console.log("["+filename+":"+lineNumber+"] "+message);
-    if (object){
+
+    var msg = "[" + filename + ":" + lineNumber + "] ";
+
+    if (debug == true) {
+        msg = "[dbg]" + msg;
+    }
+
+    return msg;
+}
+
+
+function log(message, object, debug, depth) {
+
+    if (!depth){
+        depth = 3;
+    }
+
+    var head = createMsgHead(depth, debug);
+
+    console.log(head + message);
+
+    if (object) {
         console.log(object);
     }
+}
+
+function debug(message, object){
+    if (!DEBUG_MODE_ON){
+        return;
+    }
+
+    log(message, object, true, 4);
 }
