50799827f0493d4747bf9d097edeb4289d76b8f1
santiagocasti <santiago.castineira@gmail.com>
2014-06-11 16:58:21 +0200

    Added logic to communicate the value of the online user counter to the front end when the app is launched. Added logic to increase this counter and replicate it after the sockets for replicating this have been created. This will need to be debugged because events can happen in different order.

--- background.js
+++ background.js
@@ -2,6 +2,11 @@ chrome.app.runtime.onLaunched.addListener(function (launchData) {
     chrome.app.window.create('index.html', function (win) {
         win.contentWindow.launchData = launchData;
     });
+
+    var c = Context.getInstance();
+    var counter = c.getOnlineUsersCounter();
+    var appController = ApplicationController.getInstance();
+    appController.updateOnlineUserCount(counter.getCount());
 });
 
 var n = Network.getInstance();
@@ -21,12 +26,26 @@ promise.then(function () {
     });
 
     debug("Starting the socket creation part....");
-    return n.createMulticastSocket(n.UDP_TYPE, ReplicationP.Port, replicationDataReceived);
-});
+    n.createMulticastSocket(n.UDP_TYPE, ReplicationProtocol.Port, replicationDataReceived);
 
+}).then(function () {
+            debug("Starting the online counter manipulation part");
+            var counter = CRDT.newCounter(1, {});
+            var c = Context.getInstance();
+
+            counter.increment(c.getHashedReplicaId());
+
+            c.setOnlineUsersCounter(counter);
+
+            var appController = ApplicationController.getInstance();
+            appController.updateOnlineUserCount(counter.getCount());
+
+            ReplicationController.ReplicateCounter(counter);
+
+        });
 
 // Add a listener to the internal message passing mechanism of the app
-function callback_oAk9bgKDjyjd(message, sender, sendResponse){
+function callback_oAk9bgKDjyjd(message, sender, sendResponse) {
     BackEndMessaging.handleMessage(message, sender, sendResponse);
 }
 chrome.runtime.onMessage.addListener(callback_oAk9bgKDjyjd);
