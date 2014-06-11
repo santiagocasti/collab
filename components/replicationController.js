20de66c234d2971f432591c2ab2c42496ef5d0a1
santiagocasti <santiago.castineira@gmail.com>
2014-06-11 17:09:24 +0200

    Logic for handling the counter replication messages and merging it to the corresponding counter. Then, the application controller communicates to frontend.

--- components/replicationController.js
+++ components/replicationController.js
@@ -12,7 +12,26 @@ var ReplicationController = (function () {
             debug("Replication controller, replicating:", msg);
 
             var n = Network.getInstance();
-            n.sendMulticastMessage(ReplicationP.MulticastIP, ReplicationP.Port, msg, callback);
+            n.sendMulticastMessage(ReplicationProtocol.MulticastIP, ReplicationProtocol.Port, msg, callback);
+        },
+
+        ReplicateCounter: function (counter) {
+
+            debug("Replicating counter: ", counter.toJSON());
+
+            var payload = ReplicationPayload.new(
+                    ReplicationProtocol.PayloadTypes.COUNTER,
+                    counter.getId(),
+                    counter.toJSON()
+            );
+
+            var msg = Message.Create(ReplicationProtocol.MessageTypes.OUT, payload);
+
+            var counterReplicated_r6yWxvuw84mr = (function () {
+                debug("Counter replicated: ", payload.toJSON());
+            });
+
+            ReplicationController.Replicate(msg, counterReplicated_r6yWxvuw84mr);
         },
 
         /**
@@ -23,11 +42,32 @@ var ReplicationController = (function () {
         HandleMessage: function (rawMsg) {
             debug("Replication Controller handle message: ", rawMsg);
 
-            var repMsg = Message.CreateFromRawData(ReplicationP.MessageTypes.IN, rawMsg.data);
+            var repMsg = Message.CreateFromRawData(ReplicationProtocol.MessageTypes.IN, rawMsg.data);
             debug("Replication message received:", repMsg);
 
-            var appController = ApplicationController.getInstance();
-            appController.newDataReceived(repMsg, rawMsg);
+            var payload = repMsg.getPayload();
+
+            switch (payload.getType()) {
+                case ReplicationProtocol.PayloadTypes.COUNTER:
+
+                        var newCounter = CRDT.newCounter(payload.getObjectId(), JSON.parse(payload.getContent()));
+                        var c = Context.getInstance();
+
+                        var existingCounter = c.getOnlineUsersCounter();
+                        var mergedCounter = existingCounter.merge(newCounter);
+                        c.setOnlineUsersCounter(mergedCounter);
+
+                        var count = mergedCounter.getCount();
+
+                        var appController = ApplicationController.getInstance();
+                        appController.updateOnlineUserCount(count);
+
+                    break;
+                default:
+            }
+
+
+
         }
     };
 })();
