0f0410567819a52166c1637b30839bc62ac765b5
santiagocasti <santiago.castineira@gmail.com>
2014-06-12 18:41:15 +0200

    Moving logic from background script to ApplicationController as it takes more responsibility.

--- background.js
+++ background.js
@@ -2,11 +2,6 @@ chrome.app.runtime.onLaunched.addListener(function (launchData) {
     chrome.app.window.create('index.html', function (win) {
         win.contentWindow.launchData = launchData;
     });
-
-    var c = Context.getInstance();
-    var counter = c.getOnlineUsersCounter();
-    var appController = ApplicationController.getInstance();
-    appController.updateOnlineUserCount(counter.getCount());
 });
 
 var n = Network.getInstance();
@@ -29,19 +24,8 @@ promise.then(function () {
     n.createMulticastSocket(n.UDP_TYPE, ReplicationProtocol.Port, replicationDataReceived);
 
 }).then(function () {
-            debug("Starting the online counter manipulation part");
-            var counter = CRDT.newCounter(1, {});
-            var c = Context.getInstance();
-
-            counter.increment(c.getHashedReplicaId());
-
-            c.setOnlineUsersCounter(counter);
-
             var appController = ApplicationController.getInstance();
-            appController.updateOnlineUserCount(counter.getCount());
-
-            ReplicationController.ReplicateCounter(counter);
-
+            appController.appStarted();
         });
 
 // Add a listener to the internal message passing mechanism of the app
