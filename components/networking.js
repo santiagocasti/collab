cc1fae1acfc4c0071fb8e1a6cdaa78c6d38576de
santiagocasti <santiago.castineira@gmail.com>
2014-06-12 18:46:55 +0200

    Cleaning up code.

--- components/networking.js
+++ components/networking.js
@@ -623,22 +623,10 @@ var Network = (function () {
 
                     log("IdentityString: " + identityString);
 
-                    function hash(a) {
-                        var hash = 0;
-                        if (this.length == 0) return hash;
-                        for (i = 0; i < this.length; i++) {
-                            char = this.charCodeAt(i);
-                            hash = ((hash << 5) - hash) + char;
-                            hash = hash & hash; // Convert to 32bit integer
-                        }
-                        return hash;
-                    }
-
-                    log("After hashing: "+identityString.hashCode());
-
                     var c = Context.getInstance();
                     c.setReplicaIdentity(identityString);
 
+                    log("After hashing: "+ c.getHashedReplicaId());
 
                     resolve();
                 });
