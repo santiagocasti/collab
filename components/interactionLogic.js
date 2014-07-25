211d649e960e8d840c710aa0274d12c572d7729d
santiagocasti <santiago.castineira@gmail.com>
2014-07-25 17:09:33 +0200

    Isolated the function to generate random numbers in a separate file.

--- components/interactionLogic.js
+++ components/interactionLogic.js
@@ -112,15 +112,6 @@ app.controller('InteractionCtrl', function ($scope) {
     }
 
     /**
-     * Returns a random integer between min (inclusive) and max (inclusive)
-     * Using Math.round() will give you a non-uniform distribution!
-     */
-    function getRandomInt(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-
-
-    /**
      * Method that just sets up the cells values
      */
     $scope.setupSpreadsheet = function () {
