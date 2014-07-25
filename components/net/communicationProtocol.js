cf9831b2520ed6829381c7d74bd03a5713205b54
santiagocasti <santiago.castineira@gmail.com>
2014-07-25 16:44:40 +0200

    Created a base prototype for the Communication protocols to be defined.

---
+++ components/net/communicationProtocol.js
@@ -0,0 +1,19 @@
+function CommunicationProtocol(port) {
+    this.private = {};
+    this.name = "CommunicationProtocol";
+    this.port = port;
+}
+
+CommunicationProtocol.prototype.handleMessage = function (rawMsg, socketId) {
+    console.log("ERROR: Method handleMessage(rawMsg, socketId) is not defined.");
+}
+
+CommunicationProtocol.prototype.getName = function () {
+    return this.name;
+}
+
+
+if (typeof module != 'undefined') {
+    module.exports = CommunicationProtocol;
+}
+
