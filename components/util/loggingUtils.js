fb940fc6b439cec7814c9f78a637841b969694fc
santiagocasti <santiago.castineira@gmail.com>
2014-11-09 17:52:27 +0100

    Fixing small issue with the callback in the function log_created and formating whole file.

--- components/util/loggingUtils.js
+++ components/util/loggingUtils.js
@@ -50,23 +50,25 @@ function debug(message, object) {
 function log_delivered(hash) {
 
     var p = new TimeServerProtocol();
-    p.request(function (time){
-        sudo_log("|"+time+"|delivered|"+hash, 4);
+    p.request(function (time) {
+        sudo_log("|" + time + "|delivered|" + hash, 4);
     });
 
 }
 
-function log_created(hash, callback){
+function log_created(hash, callback) {
 
     var p = new TimeServerProtocol();
-    p.request(function (time){
-        sudo_log("|"+(time)+"|created|"+hash, 4);
-        callback();
+    p.request(function (time) {
+        sudo_log("|" + (time) + "|created|" + hash, 4);
+        if (typeof callback != 'undefined') {
+            callback();
+        }
     });
 
 }
 
-function sudo_log(message, depth){
+function sudo_log(message, depth) {
     var head = createMsgHead(depth, debug);
 
     var c = Context.getInstance();
@@ -75,37 +77,37 @@ function sudo_log(message, depth){
     console.log(head + message);
 }
 
-(function(console){
+(function (console) {
 
-    console.save = function(data, filename){
+    console.save = function (data, filename) {
 
-        if(!data) {
+        if (!data) {
             console.error('Console.save: No data')
             return;
         }
 
-        if(!filename) filename = 'console.json'
+        if (!filename) filename = 'console.json'
 
-        if(typeof data === "object"){
+        if (typeof data === "object") {
             data = JSON.stringify(data, undefined, 4)
         }
 
         var blob = new Blob([data], {type: 'text/json'}),
-            e    = document.createEvent('MouseEvents'),
-            a    = document.createElement('a')
+                e = document.createEvent('MouseEvents'),
+                a = document.createElement('a')
 
         a.download = filename
         a.href = window.URL.createObjectURL(blob)
-        a.dataset.downloadurl =  ['text/json', a.download, a.href].join(':')
+        a.dataset.downloadurl = ['text/json', a.download, a.href].join(':')
         e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
         a.dispatchEvent(e)
     }
 
-    console.downloadLogFile = function (){
+    console.downloadLogFile = function () {
         var c = Context.getInstance();
         var n = Network.getInstance();
         var date = new Date();
 
-        console.save(c.getDeliveryLog(), "Log_"+ n.getVPNIp() +".txt");
+        console.save(c.getDeliveryLog(), "Log_" + n.getVPNIp() + ".txt");
     }
 })(console);
